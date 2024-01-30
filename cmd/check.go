ae4bdc6e0c4d8f0b64a25f842830461365a97d5d
Liam Trowel <trowel.liam@gmail.com>
2024-01-30 16:32:38 -0500

    Rename done to check

--- cmd/check.go
+++ cmd/check.go
@@ -14,16 +14,16 @@ import (
 	"github.com/spf13/viper"
 )
 
-// doneCmd represents the done command
-var doneCmd = &cobra.Command{
-	Use:     "done",
+// checkCmd represents the check command
+var checkCmd = &cobra.Command{
+	Use:     "check",
 	Short:   "Mark an Item as Done",
 	Aliases: []string{"do"},
 	Long:    `Mark a To-Do Item as completed`,
-	Run:     doneRun,
+	Run:     checkRun,
 }
 
-func doneRun(cmd *cobra.Command, args []string) {
+func checkRun(cmd *cobra.Command, args []string) {
 	items, _ := list.ReadItems(viper.GetString("datafile"))
 	i, err := strconv.Atoi(args[0])
 
@@ -43,15 +43,15 @@ func doneRun(cmd *cobra.Command, args []string) {
 }
 
 func init() {
-	rootCmd.AddCommand(doneCmd)
+	rootCmd.AddCommand(checkCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// doneCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// checkCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// doneCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// checkCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
